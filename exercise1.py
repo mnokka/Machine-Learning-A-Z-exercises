82d5b617433f450155320868d86231c526db91f0
Mika Nokka <mika.nokka1@gmail.com>
2025-01-23 11:40:20 +0200

    Exercise1 done

--- exercise1.py
+++ exercise1.py
@@ -4,3 +4,11 @@ import numpy as np  # Numerical operations
 from sklearn.model_selection import train_test_split # Dataset splitting
 
 
+# Importing the dataset
+dataset = pd.read_csv('iris.csv')
+
+# Creating the matrix of features (X) and the dependent variable vector (y)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
