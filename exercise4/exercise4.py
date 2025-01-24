7853cfe60cc17ad25d54114282983a6c923fe802
Mika Nokka <mika.nokka1@gmail.com>
2025-01-24 12:52:58 +0200

    Exercise updated. Passes autotest.

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import StandardScaler
 dataset = pd.read_csv('iris.csv')
 
 # Separate features and target
-X = dataset.drop(columns=['target'])  # Piirteet, kaikki sarakkeet paitsi 'target'
+X = dataset.drop('target',axis=1)  # Piirteet, kaikki sarakkeet paitsi 'target'
 y = dataset['target']  # Tulosarvo (target)
 
 # Split the dataset into an 80-20 training-test set
@@ -21,12 +21,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Apply feature scaling on the training and test sets
 scaler = StandardScaler()
 
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_train= scaler.fit_transform(X_train)
+X_test= scaler.transform(X_test)
 
-# Print the scaled training and test sets
-print("Skaalatut koulutusjoukon piirteet (X_train_scaled):")
-print(X_train_scaled)
-
-print("Skaalatut testijoukon piirteet (X_test_scaled):")
-print(X_test_scaled)
+# Print scaled training and test sets
+print("Scaled Training Set:")
+print(X_train)
+print("\nScaled Test Set:")
+print(X_test)
